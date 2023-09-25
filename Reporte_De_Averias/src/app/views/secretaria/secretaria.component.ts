f44166125728f3fbc70e5010f8dc75d81b4fb882
Titicc <83614737+Titicc@users.noreply.github.com>
2023-09-25 17:45:36 -0600

    Creación de la pantalla general de cualquier rol

--- Reporte_De_Averias/src/app/views/secretaria/secretaria.component.ts
+++ Reporte_De_Averias/src/app/views/secretaria/secretaria.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {NgIf, NgFor} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-secretaria',
   templateUrl: './secretaria.component.html',
-  styleUrls: ['./secretaria.component.css']
+  styleUrls: ['./secretaria.component.css'],
+  standalone:true,
+  imports: [
+    NgIf,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    NgFor,
+  ],
 })
 export class SecretariaComponent {
 
+
+  mobileQuery: MediaQueryList;
+
+  //fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  fillerNav=[
+    {name:"Crear reporte",route:"",icon:"perm_contact_calendar"},
+    {name:"Listar reportes",route:"",icon:"perm_contact_calendar"}
+
+
+  ]
+
+  fillerContent = Array.from(
+    {length: 50},
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
 }
+
+
