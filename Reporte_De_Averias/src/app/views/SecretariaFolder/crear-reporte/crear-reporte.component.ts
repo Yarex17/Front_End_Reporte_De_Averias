1a81702cda123cc3ac7ef729269bdf31d6c07f65
Titicc <83614737+Titicc@users.noreply.github.com>
2023-09-26 00:38:27 -0600

    acomodo de vistas secretaria

---
+++ Reporte_De_Averias/src/app/views/SecretariaFolder/crear-reporte/crear-reporte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-reporte',
+  templateUrl: './crear-reporte.component.html',
+  styleUrls: ['./crear-reporte.component.css']
+})
+export class CrearReporteComponent {
+
+}
