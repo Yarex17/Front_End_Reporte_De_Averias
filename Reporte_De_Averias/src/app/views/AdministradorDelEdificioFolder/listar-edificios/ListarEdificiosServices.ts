63fc99e1b650d08e239c10b906a8faa7b6f6c958
Titicc <83614737+Titicc@users.noreply.github.com>
2023-10-10 07:05:08 -0600

    asd

--- Reporte_De_Averias/src/app/views/AdministradorDelEdificioFolder/listar-edificios/ListarEdificiosServices.ts
+++ Reporte_De_Averias/src/app/views/AdministradorDelEdificioFolder/listar-edificios/ListarEdificiosServices.ts
@@ -16,4 +16,15 @@ export class ListarEdificioServices{
         return this.http.get<Edificio[]>('https://localhost:7196/api/TraEdificios/ListarTraEdificio');
     }
 
+    registrarEdificio(data: Edificio): Observable<any> {
+        return this.http.post('https://localhost:7196/api/TraEdificios/RegistrarTraEdificio',data);
+      }
+
+      modificarEdificio(data: Edificio): Observable<any> {
+        return this.http.post('https://localhost:7196/api/TraEdificios/ModificarTraEdificio',data);
+      }
+      eliminarEdificio(data: any): Observable<any> {
+        return this.http.post('https://localhost:7196/api/TraEdificios/EliminarTraEdificio',data);
+      }
+
 }
